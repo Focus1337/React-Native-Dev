215356057607cd0190b4ea0b5c07885c2de307ff
Focus1337 <maniaclord@bk.ru>
2023-09-28 18:41:46 +0300

    Practice 3: fourth task

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import FirstTask from "./tasks/FirstTask";
 import SecondTask from "./tasks/SecondTask";
 import ThirdTask from "./tasks/ThirdTask";
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import FourthTask from "./tasks/FourthTask";
 
+const Tab = createBottomTabNavigator();
 
 function App() {
     return (
-        // <FirstTask/>
-        // <SecondTask/>
-        <ThirdTask/>
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{headerShown: false}}>
+                <Tab.Screen name={'First'} component={FirstTask}/>
+                <Tab.Screen name={'Second'} component={SecondTask}/>
+                <Tab.Screen name={'Third'} component={ThirdTask}/>
+                <Tab.Screen name={'Fourth'} component={FourthTask}/>
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
 
