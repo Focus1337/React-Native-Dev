daf94e1c5abf5a710ea0e20a5494e131db6afcd3
Focus <maniaclord@bk.ru>
2023-12-10 19:15:32 +0300

    hw-9 deep link

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {Linking, StatusBar} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {MainScreen} from "./screens/todo/MainScreen";
 import {TodoItemScreen} from "./screens/todo/TodoItemScreen";
@@ -7,11 +7,21 @@ import {DoneListScreen} from "./screens/todo/DoneListScreen";
 import {NativeStackParamList} from "./utils/types";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {LogsScreen} from "./screens/log/LogsScreen";
+import Navigation from "./utils/navigation/Navigation";
+import {DeepLinking} from "./utils/navigation/DeepLinking";
 
 const Stack = createNativeStackNavigator<NativeStackParamList>();
 export default function App() {
+    useEffect(() => {
+        Linking.getInitialURL().then(async deepLinkInitialURL => {
+            if (deepLinkInitialURL) {
+                await DeepLinking.handleInitialNavigate(deepLinkInitialURL);
+            }
+        });
+    }, []);
+
     return (
-        <NavigationContainer>
+        <NavigationContainer linking={DeepLinking.linking} ref={Navigation.navigationRef}>
             <Stack.Navigator initialRouteName={'Main'}>
                 <Stack.Screen name={'Main'} component={MainScreen} options={{headerShown: false}}/>
                 <Stack.Screen name={'TodoItem'} component={TodoItemScreen}
--- utils/env.ts
+++ utils/env.ts
@@ -1,1 +1,3 @@
 export const API_URL = "https://jsonplaceholder.typicode.com";
+
+export const linkingPrefix = 'todoapp://' ;
